72c4d4aa7572040220cd803add7cbb631325b384
Borja Penuelas <bmpenuelas@gmail.com>
2019-04-05 00:08:06 +0200

    First poc version working

---
+++ src/extension.ts
@@ -0,0 +1,134 @@
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('waveformRender.start', () => {
+			WaveformRenderPanel.createOrShow(context.extensionPath);
+		})
+	);
+}
+
+/**
+ * Manages webview panels
+ */
+class WaveformRenderPanel {
+	/**
+	 * Track the currently panel. Only allow a single panel to exist at a time.
+	 */
+	public static currentPanel: WaveformRenderPanel | undefined;
+
+	public static readonly viewType = 'waveformRender';
+
+	private readonly _panel: vscode.WebviewPanel;
+	private readonly _extensionPath: string;
+	private _disposables: vscode.Disposable[] = [];
+
+	public static createOrShow(extensionPath: string) {
+		const column = vscode.window.activeTextEditor
+			? vscode.window.activeTextEditor.viewColumn
+			: undefined;
+
+		// If we already have a panel, show it.
+		if (WaveformRenderPanel.currentPanel) {
+			WaveformRenderPanel.currentPanel._panel.dispose();
+		}
+
+		// Otherwise, create a new panel.
+		const panel = vscode.window.createWebviewPanel(
+			WaveformRenderPanel.viewType,
+			'waveform render',
+			column || vscode.ViewColumn.One,
+			{
+				// Enable javascript in the webview
+				enableScripts: true,
+
+				// And restrict the webview to only loading content from our extension's `localScripts` directory.
+				localResourceRoots: [vscode.Uri.file(path.join(extensionPath, 'localScripts'))]
+			}
+		);
+
+		WaveformRenderPanel.currentPanel = new WaveformRenderPanel(panel, extensionPath);
+	}
+
+	private constructor(panel: vscode.WebviewPanel, extensionPath: string) {
+		this._panel = panel;
+		this._extensionPath = extensionPath;
+
+		// Get the current text editor 
+		let editor = vscode.window.activeTextEditor; 
+		let doc = editor.document; 
+		let docContent = doc.getText(); 
+
+		// Set the webview's initial html content
+		this._update(docContent);
+
+		// Listen for when the panel is disposed
+		// This happens when the user closes the panel or when the panel is closed programatically
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+	}
+
+	public dispose() {
+		WaveformRenderPanel.currentPanel = undefined;
+
+		// Clean up our resources
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x) {
+				x.dispose();
+			}
+		}
+	}
+
+	private _update(fileContents: string = `{ signal: [
+		{ name: "clk",         wave: "p.....|..." },
+		{ name: "Data",        wave: "x.345x|=.x", data: ["head", "body", "tail", "data"] },
+		{ name: "Request",     wave: "0.1..0|1.0" },
+		{},
+		{ name: "Acknowledge", wave: "1.....|01." }
+	  ]}`) {
+		this._panel.webview.html = this._getHtmlForWebview(fileContents);
+	}
+
+	private _getHtmlForWebview(waveformJson: string) {
+
+		const scriptPathOnDisk = vscode.Uri.file(
+			path.join(this._extensionPath, 'localScripts', 'wavedrom.min.js')
+		);
+		const defaultSkinPathOnDisk = vscode.Uri.file(
+			path.join(this._extensionPath, 'localScripts/skins', 'default.js')
+		);
+		const narrowSkinPathOnDisk = vscode.Uri.file(
+			path.join(this._extensionPath, 'localScripts/skins', 'narrow.js')
+		);
+
+		// And the uri we use to load this script in the webview
+		const scriptUri = scriptPathOnDisk.with({ scheme: 'vscode-resource' });
+		const defaultUri = defaultSkinPathOnDisk.with({ scheme: 'vscode-resource' });
+		const narrowUri = narrowSkinPathOnDisk.with({ scheme: 'vscode-resource' });
+
+		return `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+
+								<script src="${scriptUri}"></script>
+								<script src="${defaultUri}"></script>
+								<script src="${narrowUri}"></script>
+
+                <title>waveform render</title>
+						</head>
+						
+						<body onload="WaveDrom.ProcessAll()">
+						  <div style="background-color: white;">
+								<script type="WaveDrom">
+									${waveformJson}
+								</script>
+							</div>
+            </body>
+            </html>`;
+	}
+}
