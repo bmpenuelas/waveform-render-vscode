9026ceb7c97209e08938c4d5ff7cf7f92291d016
Borja Penuelas <bmpenuelas@gmail.com>
2022-03-24 15:18:04 +0100

    Live Preview now tracks changes only in the file where it was activated (#9)
    It prevents rendering other files if you switch files while working on a waveform. This commit also fixes the issue where it maximized on reveal in some devices (#13).

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,33 @@
-import * as path from 'path';
-import * as vscode from 'vscode';
-
+import * as path from "path";
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand('waveformRender.start', () => {
+    vscode.commands.registerCommand("waveformRender.start", () => {
+      WaveformRenderPanel.disableLivePreview();
+      vscode.window.showInformationMessage(
+        "Waveform refreshed manually, Live Preview OFF"
+      );
       WaveformRenderPanel.createOrShow(context.extensionPath);
     })
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand('waveformRender.toggleLivePreview', () => {
+    vscode.commands.registerCommand("waveformRender.toggleLivePreview", () => {
       WaveformRenderPanel.toggleLivePreview(context.extensionPath);
     })
   );
 }
 
-  function getTitle() {
-    return 'Waveform Render: ' + vscode.window.activeTextEditor.document.fileName.split('\\').pop().split('/').pop();
-  }
+function getTitle() {
+  return (
+    "Waveform Render: " +
+    vscode.window.activeTextEditor.document.fileName
+      .split("\\")
+      .pop()
+      .split("/")
+      .pop()
+  );
+}
 
 /**
  * Manages webview panel
@@ -29,9 +39,10 @@ class WaveformRenderPanel {
   public static currentPanel: WaveformRenderPanel | undefined;
 
   public static livePreview: boolean = false;
+  public static livePreviewDocumentPath;
   public static listenerTextChange;
 
-  public static readonly viewType = 'waveformRender';
+  public static readonly viewType = "waveformRender";
 
   private readonly _panel: vscode.WebviewPanel;
   private readonly _extensionPath: string;
@@ -39,44 +50,67 @@ class WaveformRenderPanel {
 
   public static toggleLivePreview(extensionPath: string) {
     if (WaveformRenderPanel.livePreview) {
-      WaveformRenderPanel.listenerTextChange.dispose();
+      WaveformRenderPanel.disableLivePreview();
     } else {
-      WaveformRenderPanel.listenerTextChange = vscode.workspace.onDidChangeTextDocument(function (event) {
-        WaveformRenderPanel.createOrShow(extensionPath);
-      });
+      WaveformRenderPanel.livePreviewDocumentPath =
+        vscode.window.activeTextEditor.document.uri.path;
+      WaveformRenderPanel.listenerTextChange =
+        vscode.workspace.onDidChangeTextDocument(function (event) {
+          WaveformRenderPanel.createOrShow(extensionPath);
+        });
+      WaveformRenderPanel.livePreview = true;
     }
-    WaveformRenderPanel.livePreview = !WaveformRenderPanel.livePreview
-    vscode.window.showInformationMessage('Waveform live preview: ' + ((WaveformRenderPanel.livePreview)? 'ON' : 'OFF'));
+    vscode.window.showInformationMessage(
+      "Waveform Live Preview: " +
+        (WaveformRenderPanel.livePreview ? "ON" : "OFF")
+    );
+  }
+
+  public static disableLivePreview() {
+    WaveformRenderPanel.livePreviewDocumentPath = null;
+    if (WaveformRenderPanel.listenerTextChange) {
+      WaveformRenderPanel.listenerTextChange.dispose();
+    }
+    WaveformRenderPanel.livePreview = false;
   }
 
   public static createOrShow(extensionPath: string) {
-    const column = vscode.window.activeTextEditor
-      ? vscode.window.activeTextEditor.viewColumn
-      : undefined;
-
     // If we already have a panel, show it.
     if (WaveformRenderPanel.currentPanel) {
-      WaveformRenderPanel.currentPanel._panel.reveal(undefined, true);
-      WaveformRenderPanel.currentPanel._panel.title = getTitle();
-      WaveformRenderPanel.currentPanel._updateWithFileContent();
-      return
+      // If live preview is on, only update the document where it was activated
+      if (
+        WaveformRenderPanel.livePreview &&
+        WaveformRenderPanel.livePreviewDocumentPath !=
+          vscode.window.activeTextEditor.document.uri.path
+      ) {
+        return;
+      } else {
+        WaveformRenderPanel.currentPanel._panel.title = getTitle();
+        WaveformRenderPanel.currentPanel._updateWithFileContent();
+        return;
+      }
     }
 
     // Otherwise, create a new panel.
     const panel = vscode.window.createWebviewPanel(
       WaveformRenderPanel.viewType,
       getTitle(),
-      {preserveFocus: true, viewColumn: -2},
+      { preserveFocus: true, viewColumn: -2 },
       {
         // Enable javascript in the webview
         enableScripts: true,
 
         // And restrict the webview to only loading content from our extension's `localScripts` directory.
-        localResourceRoots: [vscode.Uri.file(path.join(extensionPath, 'localScripts'))]
+        localResourceRoots: [
+          vscode.Uri.file(path.join(extensionPath, "localScripts")),
+        ],
       }
     );
 
-    WaveformRenderPanel.currentPanel = new WaveformRenderPanel(panel, extensionPath);
+    WaveformRenderPanel.currentPanel = new WaveformRenderPanel(
+      panel,
+      extensionPath
+    );
   }
 
   private constructor(panel: vscode.WebviewPanel, extensionPath: string) {
@@ -104,16 +138,6 @@ class WaveformRenderPanel {
     }
   }
 
-  private _update(fileContents: string = `{ signal: [
-    { name: "clk",         wave: "p.....|..." },
-    { name: "Data",        wave: "x.345x|=.x", data: ["head", "body", "tail", "data"] },
-    { name: "Request",     wave: "0.1..0|1.0" },
-    {},
-    { name: "Acknowledge", wave: "1.....|01." }
-  ]}`) {
-    this._panel.webview.html = this._getHtmlForWebview(fileContents);
-  }
-
   private _updateWithFileContent() {
     // Get the current text editor
     let editor = vscode.window.activeTextEditor;
@@ -124,25 +148,39 @@ class WaveformRenderPanel {
     this._update(docContent);
   }
 
+  private _update(
+    fileContents: string = `{ signal: [
+    { name: "clk",         wave: "p.....|..." },
+    { name: "Data",        wave: "x.345x|=.x", data: ["head", "body", "tail", "data"] },
+    { name: "Request",     wave: "0.1..0|1.0" },
+    {},
+    { name: "Acknowledge", wave: "1.....|01." }
+  ]}`
+  ) {
+    this._panel.webview.html = this._getHtmlForWebview(fileContents);
+  }
+
   private _getHtmlForWebview(waveformJson: string) {
     const scriptPathOnDisk = vscode.Uri.file(
-      path.join(this._extensionPath, 'localScripts', 'wavedrom.min.js')
-      );
+      path.join(this._extensionPath, "localScripts", "wavedrom.min.js")
+    );
     const defaultSkinPathOnDisk = vscode.Uri.file(
-      path.join(this._extensionPath, 'localScripts/skins', 'default.js')
+      path.join(this._extensionPath, "localScripts/skins", "default.js")
     );
     const narrowSkinPathOnDisk = vscode.Uri.file(
-      path.join(this._extensionPath, 'localScripts/skins', 'narrow.js')
+      path.join(this._extensionPath, "localScripts/skins", "narrow.js")
     );
     const lowkeySkinPathOnDisk = vscode.Uri.file(
-      path.join(this._extensionPath, 'localScripts/skins', 'lowkey.js')
+      path.join(this._extensionPath, "localScripts/skins", "lowkey.js")
     );
 
     // And the uri we use to load this script in the webview
-    const scriptUri = scriptPathOnDisk.with({ scheme: 'vscode-resource' });
-    const defaultUri = defaultSkinPathOnDisk.with({ scheme: 'vscode-resource' });
-    const narrowUri = narrowSkinPathOnDisk.with({ scheme: 'vscode-resource' });
-    const lowkeyUri = lowkeySkinPathOnDisk.with({ scheme: 'vscode-resource' });
+    const scriptUri = scriptPathOnDisk.with({ scheme: "vscode-resource" });
+    const defaultUri = defaultSkinPathOnDisk.with({
+      scheme: "vscode-resource",
+    });
+    const narrowUri = narrowSkinPathOnDisk.with({ scheme: "vscode-resource" });
+    const lowkeyUri = lowkeySkinPathOnDisk.with({ scheme: "vscode-resource" });
 
     return `<!DOCTYPE html>
             <html lang="en">
